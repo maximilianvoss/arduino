d79621251837463840e737d78e76a67626de3c5c
Maximilian Voss <voss@adobe.com>
2015-02-04 21:03:41 +0100

    fix interrrupt

--- tetris.cpp
+++ tetris.cpp
@@ -8,6 +8,8 @@ uint8_t volatile gameOver;
 tetermino_t tetermino;
 
 ISR(TIMER1_COMPA_vect) {
+		Serial.print("Board\n");
+
 	if ( ! gameOver ) {
 		if ( move(board, &tetermino, moveDown) ) {
 			memcpy ( (void *) &board, (void *) &boardDisplay, sizeof(short) * TETRIS_BOARD_HEIGHT);
@@ -36,28 +38,28 @@ void setup()
 	pinMode(ROW_CLOCKPIN, OUTPUT);
 	pinMode(ROW_DATAPIN, OUTPUT);
 
-	// set compare match register to desired timer count:
+    cli();
+    TCCR1A = 0;
+    TCCR1B = 0;
     OCR1A = 15624;
-    // turn on CTC mode:
     TCCR1B |= (1 << WGM12);
-    // Set CS10 and CS12 bits for 1024 prescaler:
     TCCR1B |= (1 << CS10);
     TCCR1B |= (1 << CS12);
-    // enable timer compare interrupt:
     TIMSK1 |= (1 << OCIE1A);
+    sei();
 }
 
 void printBoard(uint16_t volatile *board) {
-	for ( uint8_t i = TETRIS_BOARD_HEIGHT - HEAD; i > 0 ; i-- ) {
+	for ( uint8_t i = 1; i < TETRIS_BOARD_HEIGHT; i++ ) {
 		uint16_t line = (board[i] & (1<<(TETRIS_BOARD_WIDTH + 1) ) - 1);
 		line >>=1;
 
     	digitalWrite(ROW_LATCHPIN, 0);
-		shiftOut(ROW_DATAPIN, ROW_CLOCKPIN,MSBFIRST, line);
+		shiftOut(ROW_DATAPIN, ROW_CLOCKPIN, MSBFIRST, line);
     	digitalWrite(ROW_LATCHPIN, 1);
 
     	digitalWrite(COLUMN_LATCHPIN, 0);
-		shiftOut(COLUMN_DATAPIN, COLUMN_CLOCKPIN, MSBFIRST, ~(1<<(i-2)));
+		shiftOut(COLUMN_DATAPIN, COLUMN_CLOCKPIN, MSBFIRST, ~(1<<(i-1)));
     	digitalWrite(COLUMN_LATCHPIN, 1);
 	}
 }
