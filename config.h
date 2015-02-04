615dd89909b9cb66caf3fad126f8de49689eb91a
Maximilian Voss <voss@adobe.com>
2015-02-04 22:27:42 +0100

    use standard pins for output

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 #define __CONFIG_H__
 
 #define SERIAL_BAUDRATE 115200
-/*
+
 #define LATCHPIN 3
 
 #define COLUMN_CLOCKPIN 12
@@ -10,14 +10,5 @@
 
 #define ROW_CLOCKPIN 7
 #define ROW_DATAPIN 6
-//*/
 
-#define LATCHPIN 3
-
-#define COLUMN_CLOCKPIN 7
-#define COLUMN_DATAPIN 6
-
-#define ROW_CLOCKPIN 12
-#define ROW_DATAPIN 11
-//*/
 #endif
